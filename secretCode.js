09526b55a3cd4dc4e7c65b3def0934f59e43c832
Kamil Sobieraj <kamil.sobieraj.dev@gmail.com>
2019-05-16 13:09:23 +0200

    Basic solution - console only

--- secretCode.js
+++ secretCode.js
@@ -1,1 +1,31 @@
-console.log("works");
+const pressedKeys = [];
+const sharkPassword = "shark";
+const unicornPassword = "unicorn";
+const hasselhoffPassword = "specialone";
+
+function passwordSplicer(password) {
+  pressedKeys.splice(
+    -password.length - 1,
+    pressedKeys.length - password.length
+  );
+}
+
+window.addEventListener("keyup", e => {
+  //console.log(e.key);
+  pressedKeys.push(e.key);
+  pressedKeys.splice(
+    -hasselhoffPassword.length - 1,
+    pressedKeys.length - hasselhoffPassword.length
+  );
+
+  if (pressedKeys.join("").includes(sharkPassword)) {
+    passwordSplicer(sharkPassword);
+    console.log("SHARKS!!!");
+  } else if (pressedKeys.join("").includes(unicornPassword)) {
+    passwordSplicer(unicornPassword);
+    console.log("UNICORN!!!");
+  } else if (pressedKeys.join("").includes(hasselhoffPassword)) {
+    passwordSplicer(hasselhoffPassword);
+    console.log("DAVID!!!");
+  }
+});
